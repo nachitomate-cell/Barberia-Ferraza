1cb0a96c0bab29ff24f59803b8490ffe153ae7bb
Ignacio Mateluna <nachitomate@gmail.com>
2026-04-19 19:18:56 -0400

    descripción de los cambios

--- firebase-config.js
+++ firebase-config.js
@@ -15,5 +15,6 @@ if (!firebase.apps.length) {
   firebase.initializeApp(firebaseConfig);
 }
 
-const auth = firebase.auth();
-const db   = firebase.firestore();
+const auth    = firebase.auth();
+const db      = firebase.firestore();
+const storage = firebase.storage();
